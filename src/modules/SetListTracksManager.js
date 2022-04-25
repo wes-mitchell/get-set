803be01ae15292d99e525a37834fb0ceb6919195
wes-mitchell <wmdrums@gmail.com>
2022-04-25 11:49:48 -0500

    gets track card component to populate correctly within setlist view

--- src/modules/SetListTracksManager.js
+++ src/modules/SetListTracksManager.js
@@ -5,6 +5,13 @@ const remoteURL = "http://localhost:8088"
 // Gets all set list tracks from API
 
 export const getAllSetListTracks = () => { 
-  return fetch(`${remoteURL}/setListTracks`)
+  return fetch(`${remoteURL}/setListTracks?_expand=setList&_expand=track`)
   .then(res => res.json())
 }
+
+// Get all tracks by related setlist
+
+export const getSetListTracksByCurrentSetList = (currentSetListId) => { 
+  return fetch(`${remoteURL}/setListTracks?setListId=${currentSetListId}`)
+  .then(res => res.json())
+ }
